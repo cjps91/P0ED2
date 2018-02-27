8a4e7d17be84029948190c855bcf9c6c294f907d
iCarlosPro <carlosjavier.perez@alu.uhu.es>
2018-02-27 10:19:23 +0100

    Cosas hechas

--- MC/MC.cpp
+++ MC/MC.cpp
@@ -1,1 +1,41 @@
+#include "MC.h"
+#include <iostream>
+using namespace std;
 
+template <typename T> Multiconjunto<T>::Multiconjunto()
+{
+    num=0;
+};
+
+template <typename T> bool Multiconjunto<T>::esVacio() const
+{
+    if(num == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+};
+
+template <typename T> int Multiconjunto<T>::cardinalidad() const
+{
+    return num;
+};
+
+template <typename T> void Multiconjunto<T>::anade(const T& objeto)
+{
+    c[num]=objeto;
+    num++;
+};
+
+template <typename T> void Multiconjunto<T>::elimina(const T& objeto)
+{
+
+};
+
+template <typename T> bool Multiconjunto<T>::pertenece(const T& objeto) const
+{
+
+};
